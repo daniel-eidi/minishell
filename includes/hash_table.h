356ac3b6c4cd344d4bd1c71851d24688e34d0d9a
Moacir Galdino <galdinomf@protonmail.com>
2022-09-02 11:09:17 -0300

    hashtable turned to array of lists #2

--- includes/hash_table.h
+++ includes/hash_table.h
@@ -31,7 +31,6 @@ t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstclear(t_list **lst, void (*del)(void *));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
 //----------hashtable functions----------
 int	ft_pow(int base, int exp);
 size_t	ft_strlen(const char *s);
