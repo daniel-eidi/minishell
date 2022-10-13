891b3dbe1032a8a853d25a57cdd75d860b91e88d
Moacir Galdino <galdinomf@protonmail.com>
2022-10-13 20:35:21 -0300

    is_built function fixed to prevent leaks when only redirections (no cmds and args) are entered

--- src/if_builtin.c
+++ src/if_builtin.c
@@ -6,7 +6,7 @@
 /*   By: mgaldino <mgaldino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 16:39:10 by daeidi-h          #+#    #+#             */
-/*   Updated: 2022/10/10 15:11:14 by mgaldino         ###   ########.fr       */
+/*   Updated: 2022/10/13 20:30:29 by mgaldino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	is_builtin(char **cmd)
 {
 	if (!cmd[0])
-		return (0);
+		return (3);
 	if (!ft_strcmp(cmd[0], "echo") || !ft_strcmp(cmd[0], "pwd") \
 	|| !ft_strcmp(cmd[0], "env"))
 		return (1);
@@ -30,20 +30,23 @@ void	run_builtin_fork(t_cmd *cmd_table, t_pids_pipes *aux)
 	char	**cmd;
 
 	cmd = cmd_table->cmd_and_args;
-	if (!ft_strcmp(cmd[0], "cd" ))
-		builtin_cd(cmd);
-	if (!ft_strcmp(cmd[0], "echo" ))
-		builtin_echo(cmd);
-	if (!ft_strcmp(cmd[0], "pwd" ))
-		builtin_pwd();
-	if (!ft_strcmp(cmd[0], "export" ))
-		builtin_export(cmd);
-	if (!ft_strcmp(cmd[0], "env" ))
-		builtin_env();
-	if (!ft_strcmp(cmd[0], "unset" ))
-		builtin_unset(cmd);
-	if (!ft_strcmp(cmd[0], "exit" ))
-		exit_minishell(cmd_table, aux);
+	if (cmd[0])
+	{
+		if (!ft_strcmp(cmd[0], "cd" ))
+			builtin_cd(cmd);
+		if (!ft_strcmp(cmd[0], "echo" ))
+			builtin_echo(cmd);
+		if (!ft_strcmp(cmd[0], "pwd" ))
+			builtin_pwd();
+		if (!ft_strcmp(cmd[0], "export" ))
+			builtin_export(cmd);
+		if (!ft_strcmp(cmd[0], "env" ))
+			builtin_env();
+		if (!ft_strcmp(cmd[0], "unset" ))
+			builtin_unset(cmd);
+		if (!ft_strcmp(cmd[0], "exit" ))
+			exit_minishell(cmd_table, aux);
+	}
 	free_pids_and_pipes(aux);
 	clear_data();
 }
